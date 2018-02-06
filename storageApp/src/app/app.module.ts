aff164f2b4d45af307835e5d831493b9e72d6a20
Atif Mahmud <atifmahmud101@gmail.com>
2018-02-05 19:56:05 -0500

    Import Firebase config

--- storageApp/src/app/app.module.ts
+++ storageApp/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- storageApp/src/environments/environment.ts
+++ storageApp/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDmwDONhwdnMT30yR6_JGOy3pBctW4EW68",
+    authDomain: "atif-mahmud.firebaseapp.com",
+    databaseURL: "https://atif-mahmud.firebaseio.com",
+    projectId: "atif-mahmud",
+    storageBucket: "atif-mahmud.appspot.com",
+    messagingSenderId: "1010393823488"
+  }
 };
