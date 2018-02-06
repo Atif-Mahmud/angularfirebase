47703acab13ed4fa84cb2053afc55aae031fe5f6
Atif Mahmud <atifmahmud101@gmail.com>
2018-02-05 21:06:07 -0500

    Add missing imports, routing and router-outlet

--- storageApp/src/app/app.module.ts
+++ storageApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
@@ -12,15 +12,18 @@ import { environment } from '../environments/environment';
 
 import { DropZoneDirective } from './directives/drop-zone.directive';
 import { FileUploadComponent } from './components/file-upload/file-upload.component';
+import { FileSizePipe } from './pipes/file-size.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     DropZoneDirective,
-    FileUploadComponent
+    FileUploadComponent,
+    FileSizePipe
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireStorageModule
