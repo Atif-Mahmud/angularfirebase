129495656c31c010c07565427d2452b1495c34e5
Atif Mahmud <atifmahmud101@gmail.com>
2018-02-05 20:58:44 -0500

    Design file-upload

--- storageApp/src/app/components/file-upload/file-upload.component.ts
+++ storageApp/src/app/components/file-upload/file-upload.component.ts
@@ -1,13 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage, AngularFireUploadTask } from 'angularfire2/storage';
+import { Observable } from 'rxjs/Observable';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-file-upload',
   templateUrl: './file-upload.component.html',
-  styleUrls: ['./file-upload.component.css']
+  styleUrls: ['./file-upload.component.scss']
 })
 export class FileUploadComponent implements OnInit {
 
-  constructor() { }
+  // Main Task
+  task: AngularFireUploadTask;
+
+  // Progress monitoring
+  percentage: Observable<number>;
+
+  snapshot: Observable<any>;
+
+  // Download URL
+  downloadURL: Observable<string>;
+
+  // State for dropzone CSS highlighting
+  isHovering: boolean;
+
+
+  constructor(private storage: AngularFireStorage, private db: AngularFirestore) { }
+
+  toggleHover(event: boolean) {
+  }
+
+  startUpload(event: FileList) {
+    // The File object
+    const file = event.item(0);
+
+    // Client-side validation example
+    if (file.type.split('/')[0] !== 'image') {
+      console.error('unsupported file type: (' )
+    }
+
+    // The storage path
+    const path = `test/${new Date().getTime()}_${file.name}`;
+
+    //Totally optional metadata
+    const customMetadata = { app: 'My AngularFire-powered PWA'}
+
+    // The main task
+    this.task = this.storage.upload(path, file, { customMetadata });
+
+    // Progress monitoring
+    this.percentage = this.task.percentageChanges();
+    this.snapshot = this.task.snapshotChanges().pipe(
+      tap(snap => {
+        if (snap.bytesTransferred === snap.totalBytes) {
+          // Update firestore on completion
+          this.db.collection('photos').add( { path, size: snap.totalBytes });
+        }
+      })
+    );
+
+    // The file's download URL
+    this.downloadURL = this.task.downloadURL();
+  }
+
+  // Determines if the upload task is active
+  isActive(snapshot) {
+    return snapshot.state === 'running' && snapshot.bytesTransferred < snapshot.totalBytes;
+  }
 
   ngOnInit() {
   }
