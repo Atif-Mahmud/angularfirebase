f65cacf228b173e29278925c5a31ff74100b7690
Atif Mahmud <atifmahmud101@gmail.com>
2018-02-16 13:10:24 -0500

    Generate AuthService

--- emailAuth/src/app/core/core.module.ts
+++ emailAuth/src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from './auth.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [],
-  providers: []
+  providers: [AuthService]
 })
 export class CoreModule { }
