614a9f49716e5a283f30116637a8a01eafae69b6
Atif Mahmud <atifmahmud101@gmail.com>
2018-05-31 20:24:54 -0400

    Create form component scaffolds

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BasicFormComponent } from './basic-form/basic-form.component';
+import { NestedFormComponent } from './nested-form/nested-form.component';
+import { ArrayFormComponent } from './array-form/array-form.component';
+import { ValidFormComponent } from './valid-form/valid-form.component';
+import { SubmitFormComponent } from './submit-form/submit-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicFormComponent,
+    NestedFormComponent,
+    ArrayFormComponent,
+    ValidFormComponent,
+    SubmitFormComponent
   ],
   imports: [
     BrowserModule,
